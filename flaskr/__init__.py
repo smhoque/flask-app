941f43601ab6225497f96cc60d5e0cbb365971a3
emdadripon <eriponctg@gmail.com>
2024-12-17 11:44:32 -0500

    flask-application

---
+++ flaskr/__init__.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask, app
+from . import db
+from . import auth
+
+
+
+def create_app(test_config=None):
+    # Create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flask.sqlite'),
+    )
+    if test_config is None:
+        # load the instance config, if it exists, when not testinng
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/hello")
+    def hello():
+        return 'Hello, World!'
+    db.init_app(app)
+    app.register_blueprint(auth.bp)
+    return app
+
+
